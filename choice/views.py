ddae36ab4e7060c83e5078a20453ff2b7d16c321
wzpxcn <wzpxcn@hotmail.com>
2018-04-24 11:09:03 +0800

    two

--- choice/views.py
+++ choice/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import HttpResponse
 from .models import Tiku
 # Create your views here.
 
 def index(request):
-    tk_list=Tiku.objects.all()
-    return render(request,'choice/xz.html',context={'tk_list':tk_list})
+    tm=Tiku.objects.all()
+    return render(request,'choice/xz.html',{'tm':tm})
 
 
 #     return HttpResponse('ok')
