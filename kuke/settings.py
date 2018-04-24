28165370692d71be53ff4c92470a734f9d92e0ed
wzpxcn <wzpxcn@hotmail.com>
2018-04-24 15:27:16 +0800

    增加图片显示

--- kuke/settings.py
+++ kuke/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'choice'
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
